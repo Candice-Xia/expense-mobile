3ab433202be113b3fd48c12985c96cd497e1fab7
Candice Xia <Candice_Xia@Kingston.com.cn>
2025-08-31 21:28:03 +0800

    init commit

--- src/baseUrl.js
+++
@@ -1,9 +0,0 @@
-let baseUrl = "/";
-switch (process.env.NODE_ENV) {
-    case 'production':
-        baseUrl = window.location.href;
-        var ind = baseUrl.toLowerCase().lastIndexOf("/mobile/");
-        baseUrl = baseUrl.substring(baseUrl.indexOf('/', 9), ind + 1);
-        break
-}
-export default baseUrl;
